9c1f367cc9a636bcbf030eeef2b8f81ee99ff6f3
NhanIT-web <nhann5674@gmail.com>
2026-09-17 19:45:54 +0700

    Delete const login = require(". directory

--- const login = require("./auth.smoke.test.js
+++
@@ -1,9 +0,0 @@
-const login = require("./auth");
-
-describe("Smoke Test - Login", () => {
-
-    test("Đăng nhập thành công với admin/123", () => {
-        expect(login("admin", "123")).toBe(true);
-    });
-
-});
